fadf147269d1cf48e25246967d4164bcaac675e1
radek_random <jozwa.zawadiaka@gmail.com>
2020-01-08 19:16:04 +0100

    minor udpate

--- mychan/settings.py
+++ mychan/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4sxi12$-hp51^phiima5ie3p0-u16rpe2#40!i*+=92o+dl3k$'
+#SECRET_KEY = 'removed'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
